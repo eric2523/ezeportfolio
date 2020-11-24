8a86d99cd26ee8ef904cd3cc60b3122c2f449061
eric2523@gmail.com <eric2523@gmail.com>
2020-11-23 19:34:04 -0800

    Add styling

--- frontend/components/splash.jsx
+++ frontend/components/splash.jsx
@@ -2,8 +2,17 @@ import React from "react"
 
 export const Splash = (props) => {
   return (
-    <div>
-      Splash
+    <div className="splash-main">
+      <div className="splash-left">
+        <div className="splash-img">
+        </div>
+        <div className="splash-img-text">
+          <h1>Eric Xian.</h1> 
+        </div>
+      </div>
+      <div className="splash-right">
+        <h2>Software Engineer based in the Bay Area.</h2>
+      </div>
     </div>
   )
 }
