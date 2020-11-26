19633ea3b9fcb58a8676900f35a25416302623dc
eric2523@gmail.com <eric2523@gmail.com>
2020-11-25 19:09:59 -0800

    Restyle front page

--- frontend/components/splash.jsx
+++ frontend/components/splash.jsx
@@ -4,16 +4,18 @@ export const Splash = (props) => {
   return (
     <div className="splash-main">
       <div className="splash-left">
-        <div className="splash-img">
-        </div>
         <div className="splash-img-text">
           <h1>Eric Xian.</h1> 
         </div>
       </div>
       <div className="splash-right">
-        <span>- Introduction</span>
-        <h2>Software Engineer based in the Bay Area.</h2>
-        <h2>My story</h2>
+        <div className="splash-right-content">
+          <span>- Introduction</span>
+          <h2>Software Engineer based in the Bay Area.</h2>
+          <span>
+            Professional Googler
+          </span>
+        </div>
       </div>
     </div>
   )
