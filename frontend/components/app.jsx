7a5b23dea81b253dd470152945bd95dafedc66f2
eric2523@gmail.com <eric2523@gmail.com>
2020-12-20 17:14:38 -0800

    add links

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React from "react";
 import { Switch, Redirect, Route } from "react-router-dom";
 import { Splash } from "./splash";
 
@@ -6,12 +6,29 @@ export const App = () => {
   return (
     <div>
       <Switch>
-        <Route exact path="/" component={ Splash }/>
-        <Redirect to ="/"/>
+        <Route exact path="/" component={Splash} />
+        <Redirect to="/" />
       </Switch>
       <footer>
-        <span><i class="fab fa-github"></i></span>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://github.com/eric2523"
+        >
+          <span>
+            <i class="fab fa-github"></i>
+          </span>
+        </a>
+        <a
+          href="https://www.linkedin.com/in/eric-xian-2998061a6/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <span>
+            <i class="fab fa-linkedin"></i>
+          </span>
+        </a>
       </footer>
     </div>
-  )
-}
+  );
+};
